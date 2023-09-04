677ec6f43c4359652abce717f53c81fa48b771f7
aarsharma93 <sharma93@illinois.edu>
2023-09-03 23:28:28 -0500

    rm blinky and change to 15x9

--- Firmware/update_LibraryProjectFiles.sh
+++ Firmware/update_LibraryProjectFiles.sh
@@ -4,7 +4,5 @@ unzip project_files.zip
 
 cp project_files/mbed/.*project mbed
 cp project_files/common/.*project common
-cp project_files/wheel/.*project wheel
-cp project_files/blinky/.*project blinky
 rm -r project_files
-echo "Common, wheel, and MBED Project files updated"
+echo "Common and MBED Project files updated"
