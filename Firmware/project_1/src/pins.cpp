38246fb68a9685cd03a2330a8820977b4102ae03
Michael Shi <michaelshi@wirelessprv-10-195-144-49.near.illinois.edu>
2023-09-13 20:04:02 -0500

    Pushing Project 1

--- Firmware/project_1/src/pins.cpp
+++ Firmware/project_1/src/pins.cpp
@@ -22,4 +22,5 @@ DigitalOut led1(P_LED1);
 DigitalOut led2(P_LED2);
 DigitalOut led3(P_LED3);
 DigitalOut led4(P_LED4);
+DigitalOut led5(P_LED5);
 // PROJECT 1 - You can instantiate your DigitalOut object here
