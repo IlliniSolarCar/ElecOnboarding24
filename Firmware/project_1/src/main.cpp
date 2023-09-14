d488556fa777810dbe49877b8e7981a0770aaf08
hrishid101 <70420326+hrishid101@users.noreply.github.com>
2023-09-13 21:40:12 -0500

    Updated setup files and main.cpp to blink LED on pin 4 every second

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -41,7 +42,7 @@ void setup() {
 	//set up the CAN interrupts and handling.
 	common.setupCAN();
 	//set up LEDs and turn them all off
-	common.setupLEDs(&led1, &led2, &led3, &led4);
+	common.setupLEDs(&led1, &led2, &led3, &led4, &myled);
 
 	//Set Callbacks:
 	//These are side tasks (up to 8) that are run independently of the main
@@ -96,8 +97,15 @@ int main() {
         }
 
         //task 1
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
+        if(timing.tickThreshold(last_task_1_time, LED_BLINK_RATE)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if(myled.read() == 0){
+        		myled.write(1);
+        	}
+        	else{
+        		myled.write(0);
+        	}
+
         }
 
 
