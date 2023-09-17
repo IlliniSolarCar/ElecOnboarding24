22108bb259123ed0ac3e763c5c2c41578dea7aa6
Grant Ross <grantross@Grants-MacBook-Pro-9.local>
2023-09-16 19:34:46 -0500

    firmware project

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -98,6 +99,7 @@ int main() {
         //task 1
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        		led5 = !led5;
         }
 
 
