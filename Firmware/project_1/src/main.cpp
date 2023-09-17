9dff487e2ad22102b3999b661a62b7a47d973c67
Adarsh Rallabandi <arall2@illinois.edu>
2023-09-17 15:08:07 -0500

    Firmware project 1 done

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -98,6 +98,12 @@ int main() {
         //task 1
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	//reads the current value of the LED and changes it to the opposite
+        	if(led0.read() == 0){
+        		led0.write(1);
+        	} else{
+        		led0.write(0);
+        	}
         }
 
 
