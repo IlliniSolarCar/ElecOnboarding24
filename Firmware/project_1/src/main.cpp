a256dcc01eeae870eca9e7d59474f73777660d58
Jack Chang <jackc6@illinois.edu>
2023-09-25 12:22:07 -0500

    Jack Chang Firmware proj1 and Hw

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,8 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
+#include "setup.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -98,6 +100,11 @@ int main() {
         //task 1
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if(led5.read() == 0){
+        		led5.write(1);
+        	} else {
+        		led5.write(0);
+        	}
         }
 
 
