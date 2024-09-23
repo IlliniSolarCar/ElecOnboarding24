8fb8c352e86fd441d519bf439fc0459cbdae5857
CWH06 <wenheng2@illinois.edu>
2024-09-23 16:24:54 -0500

    David Chen project 1 complete

--- Firmware/update_LibraryProjectFiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-unzip project_files.zip
-
-cp project_files/mbed/.*project mbed
-cp project_files/common/.*project common
-rm -r project_files
-echo "Common and MBED Project files updated"
--- README.md
+++
@@ -1,4 +0,0 @@
-# ElecOnboarding24
-This is used for Electrical Onboarding for the Fall 2024 Semester
-
-We are excited to get to work with you guys!
