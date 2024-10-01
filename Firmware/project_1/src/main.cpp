8b4ad838752d20799230bb0980f9677f0d9c71d4
benzhao21 <bz22@illinois.edu>
2024-09-30 19:55:09 -0500

    Firwmare Easy Project Done

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,7 +10,7 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
-
+#include "pins.h"
 
 /*
  * This is an example function. It blinks the heartbeat LED and sends
@@ -97,6 +97,11 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if(led5.read() == 0){
+        		led5.write(1);
+        	} else if(led5.read() == 1) {
+        		led5.write(0);
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
