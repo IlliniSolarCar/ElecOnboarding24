97f2d734d906c3dde409a4a9d18847979c1f40f9
kasun05 <kaiyangsun05@gmail.com>
2024-09-30 20:36:40 -0500

    Firmware Project 1

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,8 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "setup.h"
+#include "pins.h"
 
 
 /*
@@ -97,6 +99,12 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+			int r = led5.read();
+			if (r == 0) {
+				led5.write(1);
+			} else {
+				led5.write(0);
+			}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
