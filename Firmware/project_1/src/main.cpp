748dbadb43b0d578c83c6d3669ed7989685434b9
Raj Swami Kontham <rkontham24@gmail.com>
2024-09-30 19:51:05 -0500

    LED blink finished

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,7 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "pins.h"
 
 
 /*
@@ -97,6 +98,8 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	led5.write((led5.read() - 1) * -1);
+        	
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
