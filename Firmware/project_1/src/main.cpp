35ed613d1190e1c4a6ca36e1bd1c716c313de303
RitISC <rannadi2@illinois.edu>
2024-10-02 13:09:50 -0500

    Firmware Project 1 Complete

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -41,7 +42,7 @@ void setup() {
 	//set up the CAN interrupts and handling.
 	common.setupCAN();
 	//set up LEDs and turn them all off
-	common.setupLEDs(&led1, &led2, &led3, &led4);
+	common.setupLEDs(&led1, &led2, &led3, &led4, &led5);
 
 	//Set Callbacks:
 	//These are side tasks (up to 8) that are run independently of the main
@@ -97,6 +98,9 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	digitalWrite(led5, HIGH);
+        	delay(100);
+        	digitalWrite(led5, LOW);
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
