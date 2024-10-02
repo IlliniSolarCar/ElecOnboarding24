28cdf93121eaa17e2e0adb88a1bd67fda7ac2cad
BinaryKiwiz <alan.kw.shi@gmail.com>
2024-10-02 15:51:33 -0500

    Firmware project 1

--- Firmware/project_1/inc/pins.h
+++ Firmware/project_1/inc/pins.h
@@ -26,7 +26,7 @@
 #define P_LED3 P0_6
 #define P_LED4 P0_7
 // PROJECT 1 - You can define a pin macro here
-
+#define P_LED0 P0_4
 /*
  * COMMON PIN OBJECT DECLARATIONS
  */
--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.cpp"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -97,6 +98,11 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if (led0.read() == 0) {
+        		led0.write(1);
+        	} else {
+        		led0.write(0);
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
