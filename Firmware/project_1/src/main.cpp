55198297b4c7d24cff4332f60861e39580e4a9d8
advaithanand10 <aanand10@illinois.edu>
2024-10-03 00:58:16 -0500

    Hardware Project 1

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,8 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "pins.h"
+#include "setup.h"
 
 
 /*
@@ -95,8 +97,9 @@ int main() {
         	common.toggleReceiveCANLED();
         }
 
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
+        if(timing.tickThreshold(last_task_1_time, PROJ_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	ledproj1.DigitalOut(P_LEDPROJ1, 1);
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
