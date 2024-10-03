9e6143c3c3a1d7393e9879f37fcb55312a3d6cf0
Parthiv Adimulam <parthivsai2@gmail.com>
2024-10-03 15:12:54 -0500

    firmware easy project done

--- Firmware/project_1/inc/pins.h
+++ Firmware/project_1/inc/pins.h
@@ -26,7 +26,7 @@
 #define P_LED3 P0_6
 #define P_LED4 P0_7
 // PROJECT 1 - You can define a pin macro here
-
+#define P_LED5 P0_4
 /*
  * COMMON PIN OBJECT DECLARATIONS
  */
@@ -36,7 +36,7 @@ extern DigitalOut led2;
 extern DigitalOut led3;
 extern DigitalOut led4;
 // PROJECT 1 - You can declare a DigitalOut object here
-
+extern DigitalOut led5;
 /*
  * BOARD SPECIFIC PINS
  */
