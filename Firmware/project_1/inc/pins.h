1da03ba050588bc19adc9c0fc54f226b3042ada4
Joseph Temple <josephtemple@Josephs-MacBook-Pro-9.local>
2024-10-02 20:21:20 -0500

    Updated

--- Firmware/project_1/inc/pins.h
+++ Firmware/project_1/inc/pins.h
@@ -28,10 +28,12 @@
 // PROJECT 1 - You can define a pin macro here
 #define P_LED5 P0_4
 #define P_POT1 P0_1
+
+
 /*
  * COMMON PIN OBJECT DECLARATIONS
+ * I believe this is all right :)
  */
-
 extern DigitalOut led1;
 extern DigitalOut led2;
 extern DigitalOut led3;
