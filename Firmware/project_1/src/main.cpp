c6ca2c32c33d97b5580ec3e50bec3be07755a591
2Ki07 <tfang8@illinois.edu>
2024-10-02 20:31:57 -0500

    firmware1

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -10,6 +10,7 @@
 #include "CAN/can_id.h"
 #include "CAN/can_data.h"
 #include "can_buffer.h"
+#include "pins.h"
 
 
 /*
@@ -95,8 +96,17 @@ int main() {
         	common.toggleReceiveCANLED();
         }
 
-        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
+        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US))
+        {
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if (led5.read()==0)
+        	{
+        		led.write(1);
+        	}
+        	else
+        	{
+        		led.write(0);
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
