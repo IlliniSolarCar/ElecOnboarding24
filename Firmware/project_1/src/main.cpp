1ce3509f6443866a8bc1be9663fc761cc973c422
peterg4a <peterg4@illinois.edu>
2024-10-07 00:12:51 -0500

    fixed bugs

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -98,7 +98,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
-        	led.write(!led.read());
+        	led5.write(!led5.read());
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
