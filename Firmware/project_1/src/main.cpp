d4bfaf020ba986593092affbdc961b1f23c011a1
= <=>
2024-10-07 21:16:56 -0500

    Firmware projects 1 and 2 completed

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include <mbed.h>
 // PROJECT 1 - Include something here!
+#include "pins.h"
 #include "peripherals.h"
 #include "can_struct.h"
 #include "CAN/can_id.h"
@@ -97,9 +98,27 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
+        	if(led5.read() == 0)
+        	{
+        		led5.write(1);
+        	}
+        	else if(led5.read() == 1)
+        	{
+        		led5.write(0);
+        	}
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
+
+        if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US*potentiometer.read())){
+        	if(led5.read() == 0)
+        	{
+        		led5.write(1);
+        	}
+        	else if(led5.read() == 1)
+        	{
+        		led5.write(0);
+        	}
 
 
 	}
