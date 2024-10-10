9d5eae22fd38f05a9bf37f7ecf38f9e227f8f72c
Power's Macbook <powersmacbook@wirelessprvnat-172-16-240-120.near.illinois.edu>
2024-10-09 19:24:47 -0500

    changed variable name for test

--- Firmware/project_1/src/main.cpp
+++ Firmware/project_1/src/main.cpp
@@ -97,7 +97,7 @@ int main() {
 
         if(timing.tickThreshold(last_task_1_time, TASK_1_RATE_US)){
         	//PROJECT 1 - add code here to actually make the LED blink
-        	int a = led5.read();
+        	int b ß= led5.read();
         }
 
         //PROJECT 2 - use the potentiometer to change the blink rate
